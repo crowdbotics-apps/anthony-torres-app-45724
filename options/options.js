7f62a9c3dc1b369fa5398b38178d48a11e2d0265
Crowdbotics <team@crowdbotics.com>
2024-01-31 19:13:07 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "anthony-torres-app-45724",
+  url: "https://anthony-torres-app-45724.botics.co",
+  api: "https://anthony-torres-app-45724.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
